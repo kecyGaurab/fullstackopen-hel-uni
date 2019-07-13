f0663c74e7f87a209864b4703315480228d04c80
K C Gaurab <s1900524@edu.bc.fi>
2019-07-13 18:23:20 +0300

    course information step 1 & 2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,42 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Hello = () => {
-    return (
-      <div>
-        <p>Hello world</p>
-      </div>
-    )
-  }
-const WhatUp = () => {
-    return (
-      <div>
-        <p>what up!</p>
-      </div>
-    )
-  }
+const Header = props => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  );
+};
 
-const User = {
-    firstname: 'Jack',
-    lastname : 'Mack'
-}
+const Part1 = props => {
+  return (
+    <div>
+      <p><b>{props.parts[0].name}</b></p>
+      <p>Number of exercises:{props.parts[0].exercises}</p>
+    </div>
+  );
+};
+const Part2 = props => {
+  return (
+    <div>
+      <p><b>{props.parts[1].name}</b></p>
+      <p>Number of exercises:{props.parts[1].exercises}</p>
+    </div>
+  );
+};
+const Part3 = props => {
+  return (
+    <div>
+      <p><b>{props.parts[2].name}</b></p>
+      <p>Number of exercises:{props.parts[2].exercises}</p>
+    </div>
+  );
+};
 
-function nameHim(User) {
-    return User.firstname +' '+ User.lastname;
-}
+const Content = props => {
+  return (
+    <div>
+      <Part1 parts={props.parts} />
+      <Part2 parts={props.parts} />
+      <Part3 parts={props.parts} />
+    </div>
+  );
+};
 
-  const App = () => {
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello />
-        <Hello />
-        <Hello />
-        <Hello />
-        <Hello /> <span><WhatUp />  {nameHim(User)} </span>
+const Total = props => {
+  return (
+    <div>
+      <p>
+        <i>
+        Total exercises:{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+          </i>
+      </p>
+    </div>
+  );
+};
 
-      </div>
-    )
-  }
-  
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <h1>
+        <Header course={course.name} />
+      </h1>
+      <p>
+        <Content parts={course.parts} />
+      </p>
+      <p>
+        <Total parts={course.parts} />
+      </p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
