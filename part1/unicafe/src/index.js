1279d99b0ac463aa3b9552edfbd1d6b1a3a98b96
K C Gaurab <s1900524@edu.bc.fi>
2019-08-07 22:18:44 +0300

    step 3 & 4 formulas fixed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,45 +1,53 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Statistics = (props) => {
-  if (props.all === 0){
-    return (<div>
-      No feedback given
-    </div>)
+const Statistics = props => {
+  if (props.all === 0) {
+    return <div>No feedback given</div>;
   }
-return (<div>
-  <div>all {props.all}</div>
-  <div>average {props.average} %</div>
-</div>
-  )
-}
+  return (
+    <div>
+      <div>all {props.all}</div>
+      <div>average {props.average}</div>
+      <div>positive {props.positive} %</div>
+    </div>
+  );
+};
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+);
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good+bad+neutral;
-  const average = (good*1+bad*(-1)+neutral*0)*100/all;
+  const all = good + bad + neutral;
+  const average = (good * 1 + bad * -1 + neutral * 0)/all;
+  const positive = (good  * 100) / all;
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={handleGood} text ='good'/>
+      <Button onClick={handleNeutral} text ='neutral'/>
+      <Button onClick={handleBad} text ='bad'/>
 
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <Statistics all = {all} 
-       average = {average}/>
-      
-
+      <Statistics all={all} average={average} positive={positive} />
     </div>
   );
 };
