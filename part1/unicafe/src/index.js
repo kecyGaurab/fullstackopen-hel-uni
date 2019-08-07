aec068f000ca5e6e1bf724090101c022b1c4dab1
K C Gaurab <s1900524@edu.bc.fi>
2019-08-07 21:16:22 +0300

    step 3 & 4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = (props) => {
+  if (props.all === 0){
+    return (<div>
+      No feedback given
+    </div>)
+  }
+return (<div>
+  <div>all {props.all}</div>
+  <div>average {props.average} %</div>
+</div>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good+neutral+bad;
-  const average = (good*1+neutral*0+bad*(-1))/(good+neutral+bad);
-  const positive = good/all*100;
+  const all = good+bad+neutral;
+  const average = (good*1+bad*(-1)+neutral*0)*100/all;
 
   return (
     <div>
@@ -20,13 +33,12 @@ const App = () => {
 
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive feedbacks {positive} % </p>
+      <Statistics all = {all} 
+       average = {average}/>
+      
 
     </div>
   );
