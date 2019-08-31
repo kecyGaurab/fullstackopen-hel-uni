521ff604538c650d7cb1c38df9d2d98aca38fc1c
K C Gaurab <s1900524@edu.bc.fi>
2019-08-31 21:16:51 +0300

    Data for countries step2

--- part2/countries/src/Components/Manyresults.js
+++ part2/countries/src/Components/Manyresults.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Manyresults = ({filteredCountries})=> {
-   const msg= filteredCountries.length > 10?
-    <div>Too many matches, specify another filter</div>
-    : null
+//component that displays the text when the matching results are more than 10
+const Manyresults = ({filteredCountries}) => {
+  const msg = filteredCountries.length > 10
+    ? <div>Too many matches, specify another filter</div>
+    : null;
 
-    return (
-        <div>
-           {msg} 
-        </div>
-    )
-}
+  return (
+    <div>
+      {msg}
+    </div>
+  );
+};
 
-export default Manyresults
+export default Manyresults;
--- part2/countries/src/Components/ShowTenCountries.js
+++ part2/countries/src/Components/ShowTenCountries.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const ShowTenCountries = ({filteredCountries}) =>{
-    const tenCountries = filteredCountries.length > 1 && filteredCountries.length < 11 ?
-    (filteredCountries.map(ten =>(
-      
-      <div key ={ten.name}>{ten.name}</div>
-    ))):null
-    return <div>{tenCountries}</div>
-  }
-  
+//component that lists 2 to 10 matching country names
+const ShowTenCountries = ({country, filteredCountries, handleClick}) => {
+  const tenCountries = filteredCountries.length > 1 &&
+    filteredCountries.length < 11
+    ? filteredCountries.map (ten => (
+        <div key={ten.name}>
+          {ten.name}
+          {ten.index}
+          <button value={ten.name} onClick={handleClick}>show</button>
+        </div>
+      ))
+    : null;
+  return <div>{tenCountries}</div>;
+};
 
-export default ShowTenCountries
+export default ShowTenCountries;
